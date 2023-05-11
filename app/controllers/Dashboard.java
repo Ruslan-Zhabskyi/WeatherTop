05cb4d9a0ba71819af6d7eeac5ab116c08101da1
ruslanZhabskyi <ru.zhabskyi@gmail.com>
2023-05-11 21:39:18 +0100

    delete stations and reports(implemented earlier)

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -25,5 +25,16 @@ public class Dashboard extends Controller
     member.save();
     redirect ("/dashboard");
   }
+
+  public static void deleteStation (Long id)
+  {
+    Logger.info("Deleting a Station");
+    Member member = Accounts.getLoggedInMember();
+    Station station = Station.findById(id);
+    member.stations.remove(station);
+    member.save();
+    station.delete();
+    redirect ("/dashboard");
+  }
 }
 
