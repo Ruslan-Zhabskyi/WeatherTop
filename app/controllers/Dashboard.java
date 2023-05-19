3530a4bbd8eba0fac1dfa7d97581e4695c205c33
ruslanZhabskyi <ru.zhabskyi@gmail.com>
2023-05-19 22:54:15 +0100

    formatted the code

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -9,37 +9,34 @@ import models.Station;
 import play.Logger;
 import play.mvc.Controller;
 
-public class Dashboard extends Controller
-{
-  public static void index()
-  {
+public class Dashboard extends Controller {
+  public static void index() {
     Logger.info("Rendering Admin");
     Member member = Accounts.getLoggedInMember();
     List<Station> stations = member.stations;
     //The two lines bellow were copied from https://howtodoinjava.com/java/sort/java-sorting/ 1.3. Sorting with Lambda Expressions
     Comparator<Station> nameSorter = (a, b) -> a.getName().compareToIgnoreCase(b.getName());
     Collections.sort(stations, nameSorter);
-    render ("dashboard.html", stations);
-  }
-  public static void addStation (String name, double lat, double lng)
-  {
-    Logger.info ("Adding a new playlist called " + name);
-    Member member = Accounts.getLoggedInMember();
-    Station station = new Station (name,lat,lng);
-    member.stations.add(station);
-    member.save();
-    redirect ("/dashboard");
+    render("dashboard.html", stations);
   }
 
-  public static void deleteStation (Long id)
-  {
+  public static void addStation(String name, double lat, double lng) {
+    Logger.info("Adding a new playlist called " + name);
+    Member member = Accounts.getLoggedInMember();
+    Station station = new Station(name, lat, lng);
+    member.stations.add(station);
+    member.save();
+    redirect("/dashboard");
+  }
+
+  public static void deleteStation(Long id) {
     Logger.info("Deleting a Station");
     Member member = Accounts.getLoggedInMember();
     Station station = Station.findById(id);
     member.stations.remove(station);
     member.save();
     station.delete();
-    redirect ("/dashboard");
+    redirect("/dashboard");
   }
 }
 
